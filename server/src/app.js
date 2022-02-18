55f5da623580c163185f081b88a94be62af47aaf
Lori McCurry <lori.mccurry@gmail.com>
2022-02-18 16:43:57 -0600

    add morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
