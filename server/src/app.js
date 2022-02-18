5da165894a986e8c30e0bb4736176bf1210ceabc
Lori McCurry <lori.mccurry@gmail.com>
2022-02-18 15:15:26 -0600

    deploy and build scripts, public dir in server

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
@@ -11,6 +12,12 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
