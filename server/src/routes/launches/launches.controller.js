ec420a6b3464e66d0b61929904342fd782c28b02
Lori McCurry <lori.mccurry@gmail.com>
2022-02-21 12:16:50 -0600

    post input validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,26 @@ function httpGetLaunches(req, res) {
 
 function httpAddLaunch(req, res) {
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    console.log(launch);
+    return res.status(400).json({
+      error: 'Missing required launch property',
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: 'Invalid launch date.',
+    });
+  }
+
   addLaunch(launch);
   return res.status(201).json(launch);
 }
