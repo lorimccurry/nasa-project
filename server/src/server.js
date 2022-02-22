e765cb3a29e919cc4d55b7a1d3136b63cfde085a
Lori McCurry <lori.mccurry@gmail.com>
2022-02-22 16:55:29 -0600

    connect to mongodb via service, setup dotenv, add cluster deploy command

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const http = require('http');
+require('dotenv').config();
+
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
@@ -7,6 +10,7 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
