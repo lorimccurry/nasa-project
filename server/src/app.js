6a046137a2d109900d9ac9ad616139ec9285ff6f
Lori McCurry <lori.mccurry@gmail.com>
2022-02-24 15:45:13 -0600

    version api

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,9 @@ const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const app = express();
+const api = require('./routes/api');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
+const app = express();
 
 app.use(
   cors({
@@ -18,8 +17,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
